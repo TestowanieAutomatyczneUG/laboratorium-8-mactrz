80316236b78c2e48d029321a754a5756135d5ed2
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-23 19:58:15 +0100

    test3green

--- src/sample/app.py
+++ src/sample/app.py
@@ -9,6 +9,10 @@ class Food:
         r = requests.get('https://www.themealdb.com/api/json/v1/1/lookup.php?', {'i': id})
         return r.json()['meals'][0]
 
+    def listByLetter(self, letter):
+        r = requests.get('https://www.themealdb.com/api/json/v1/1/search.php?', {'f': letter})
+        return r.json()['meals']
+
 food = Food()
 
 print(food.getByName('Arrabiata'))
